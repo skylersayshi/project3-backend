b7174d49adfd915e11173bdbe9791778ce887295
giantnerd123 <payneskyler@yahoo.com>
2022-05-24 15:57:53 -0700

    likes route

--- controllers/posts.js
+++ controllers/posts.js
@@ -48,3 +48,12 @@ export const deletePost = async (req, res) =>{
 
     res.json({message: 'This post has been successfully deleted'})
 }
+
+export const likePost = async (req,res) =>{
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const post = await PostMessage.findById(id);
+    const updatePost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, {new: true})
+    res.json(updatePost);
+}
