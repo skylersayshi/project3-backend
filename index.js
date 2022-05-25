f7b886e74e5c545cded77902e41888a37dd552b2
giantnerd123 <payneskyler@yahoo.com>
2022-05-24 21:58:39 -0700

    dotenv

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import calorieRoutes from './routes/calories.js';
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
@@ -15,10 +18,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/calories', calorieRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://payneskyler:hellotest@cluster0.ovg08.mongodb.net/reactapp?authSource=admin&replicaSet=atlas-10aip4-shard-0&readPreference=primary&ssl=true';
-const PORT = process.env.PORT|| 5001;
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5001;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
