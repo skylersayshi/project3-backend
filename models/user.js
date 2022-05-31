79c9836e46d7534ec34211fa2c45b62af9ab3ecb
giantnerd123 <payneskyler@yahoo.com>
2022-05-31 16:12:08 -0700

    deleted connection branch

--- models/user.js
+++ models/user.js
@@ -1,23 +1,3 @@
-import mongoose from "mongoose";
-
-const userSchema = mongoose.Schema({
-    name: String,
-    username: String,
-    password: String,
-    DOB: Number,
-    likes: String,
-    profileImage: {
-        type: String
-    },
-    email: String,
-    aboutMe: String,
-    following: Array,
-    followers: Array
-});
-
-const User = mongoose.model('User', userSchema);
-
-export default User;
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
