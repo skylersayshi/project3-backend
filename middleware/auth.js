7dc905ae480564fe892b32c56c90f9e9a671a070
giantnerd123 <payneskyler@yahoo.com>
2022-06-02 18:06:01 -0700

    added recipe backend for redux compatibility

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,19 +4,13 @@ const auth = async (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
         console.log(token)
-        const isCustomAuth = token.length < 500;
 
         let decodedData;
-        if(token && isCustomAuth){
+        if(token){
             decodedData = jwt.verify(token, 'test');
 
             req.userId = decodedData?.id;
         } 
-        else {
-            // decodedData = jwt.decode(token);
-            // req.userId = decodedData?.sub;
-            console.log('something went wrong in controllers/users')
-        }
         next();
     }
     catch(error){
