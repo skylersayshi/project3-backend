2e75291c35b288a4ca82bd74168c62d82accc8c2
giantnerd123 <payneskyler@yahoo.com>
2022-06-03 22:54:21 -0700

    search started

--- controllers/posts.js
+++ controllers/posts.js
@@ -14,6 +14,19 @@ export const getPosts = async (req, res) =>{
     }
 }
 
+export const getPostsBySearch = async (req,res) =>{
+    const {searchQuery} = req.query;
+    try {
+        const title = new RegExp(searchQuery, 'i');
+        const posts = await PostMessage.find({title})
+        // ({ $or: [{title}, {tags: { $in: tags.split(',') }}] });
+
+        res.json({data: posts});
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
 export const createPost = async (req, res) =>{
     const post = req.body;
     const newPost = new PostMessage({...post, creator: req.userId, createdAt: new Date().toISOString()});
