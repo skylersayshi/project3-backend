ecfda24b501e22ec92e1964b1fd68777673132d8
giantnerd123 <payneskyler@yahoo.com>
2022-06-04 01:26:26 -0700

    fixed search feature sort of

--- controllers/posts.js
+++ controllers/posts.js
@@ -15,10 +15,10 @@ export const getPosts = async (req, res) =>{
 }
 
 export const getPostsBySearch = async (req,res) =>{
-    const {searchQuery} = req.query;
+    const {searchQuery, tags} = req.query;
     try {
         const title = new RegExp(searchQuery, 'i');
-        const posts = await PostMessage.find({title})
+        const posts = await PostMessage.find({ $or: [ { title }, { tags } ]});
         // ({ $or: [{title}, {tags: { $in: tags.split(',') }}] });
 
         res.json({data: posts});
